693f032cfb4346f8115a973aed6deb6c30c39b6b
Thu <thunt41@unifiedpost.com>
2025-11-07 16:21:37 +0700

    add constructor to challenge 6

--- Java/JavaFoundation/challenge_6_Inheritance_Polymorphism/Car.java
+++ Java/JavaFoundation/challenge_6_Inheritance_Polymorphism/Car.java
@@ -1,6 +1,7 @@
 package Java.JavaFoundation.challenge_6_Inheritance_Polymorphism;
 
 public class Car extends Vehicle{
+
     @Override
     public void drive(){
         System.out.println("The car is driving on the road.");
--- Java/JavaFoundation/challenge_6_Inheritance_Polymorphism/Vehicle.java
+++ Java/JavaFoundation/challenge_6_Inheritance_Polymorphism/Vehicle.java
@@ -1,7 +1,11 @@
 package Java.JavaFoundation.challenge_6_Inheritance_Polymorphism;
 public class Vehicle{
     String color;
+    //constructor
+    Vehicle(){
+        this.color= "white";
+    }
     public void drive(){
-        System.out.println("The vehicle is driving.");
+        System.out.println("The "+ color+ " vehicle is driving.");
     }
 }

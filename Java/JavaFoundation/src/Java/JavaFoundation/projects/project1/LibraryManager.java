4fea06b775d52c0d898e0bef81ee2d4a19751156
Thu <thunt41@unifiedpost.com>
2025-11-24 15:53:43 +0700

    add project 1

--- Java/JavaFoundation/src/Java/JavaFoundation/projects/project1/LibraryManager.java
+++ Java/JavaFoundation/src/Java/JavaFoundation/projects/project1/LibraryManager.java
@@ -2,23 +2,48 @@ package Java.JavaFoundation.projects.project1;
 
 import java.util.HashMap;
 
+
 public class LibraryManager {
-    //declare hashmap to store documents
+    // declare hashmap to store documents
     HashMap<String, Document> documents;
-    //constructor
-    public LibraryManager(){
-        //initialize hashmap
+
+    // constructor
+    public LibraryManager() {
+        // initialize hashmap
         documents = new HashMap<>();
         documents.put("B001", new Book("Java Programming", 2020, "John Doe"));
         documents.put("D001", new Book("Science Daily", 2021, "Jane Smith"));
-       
 
     }
-    public void searchByTitle(String title){
-if(documents.get(title).equals(title)){
-    System.out.println("Document found: " + title);
 
-}
-    }
+    public void searchByTitle(String title) {
+   for(String docKey: documents.keySet()){
+       Document doc = documents.get(docKey);
+       // tại sao từ key mà lấy ra title? đáng lẽ phải là từ object chứ nhỉ?
+       if(doc.title.equals(title)){
+           System.out.println("Found Document - ID: " + docKey + ", Title: " + doc.title + ", Year: " + doc.year);
+           return;
+       }else{
+           System.out.println("Document with title " + title + " not found.");
+       }
     
+    
+    }
+}
+
+    public void displayAll() {
+        for (String docKey : documents.keySet()) {
+            Document doc = documents.get(docKey);
+            System.out.println("ID: " + docKey + ", Title: " + doc.title + ", Year: " + doc.year);
+        }
+
+    }
+
+    public void addDocument(String id, Document doc) {
+        documents.put(id, doc);
+    }
+    //delete document by id
+    public void deleteDocument(String id) {
+        documents.remove(id);
+    }
 }
---
+++ Java/JavaFoundation/src/Java/JavaFoundation/projects/project1/Main.java
@@ -0,0 +1,18 @@
+package Java.JavaFoundation.projects.project1;
+
+public class Main {
+    public static void main(String[]args){
+      LibraryManager libraryManager = new LibraryManager();
+   //   String bookName = input("Enter book name: ");
+      libraryManager.searchByTitle("test");  
+      
+      try{libraryManager.searchByTitle("test");}
+      catch(Exception e){
+        System.out.println(e.getMessage());
+      }
+      
+      libraryManager.addDocument("B111", new Book("New book", 2024, "new author"));
+      libraryManager.displayAll();
+    }
+    
+}

62f0cd5dc7a19a8bdc699a5989210477c2c25463
Thu <thunt41@unifiedpost.com>
2025-12-01 10:53:14 +0700

    add throws, try cat exception in main

--- Java/JavaFoundation/src/Java/JavaFoundation/projects/project1/LibraryManager.java
+++ Java/JavaFoundation/src/Java/JavaFoundation/projects/project1/LibraryManager.java
@@ -15,17 +15,20 @@ public class LibraryManager {
         documents.put("D001", new Book("Science Daily", 2021, "Jane Smith"));
 
     }
-
-    public void searchByTitle(String title) {
+//throws : ném ra ngoại lệ khi không tìm thấy tài liệu
+//bất kì method muốn ném ra exception cần thỏa mãn 2 điều kiện:
+//1. method phải khai báo throws ExceptionType
+//2. nơi gọi method phải xử lý ngoại lệ (try-catch hoặc throws tiếp)
+//Dùng throws	Từ chối xử lý và chuyển trách nhiệm xử lý lỗi đó cho hàm gọi nó.
+    public void searchByTitle(String title) throws DocumentNotFoundException {
    for(String docKey: documents.keySet()){
        Document doc = documents.get(docKey);
        // tại sao từ key mà lấy ra title? đáng lẽ phải là từ object chứ nhỉ?
        if(doc.title.equals(title)){
            System.out.println("Found Document - ID: " + docKey + ", Title: " + doc.title + ", Year: " + doc.year);
            return;
-       }else{
-           System.out.println("Document with title " + title + " not found.");
        }
+throw new DocumentNotFoundException("Document with title '" + title + "' not found.");
     
     
     }
--- Java/JavaFoundation/src/Java/JavaFoundation/projects/project1/Main.java
+++ Java/JavaFoundation/src/Java/JavaFoundation/projects/project1/Main.java
@@ -3,13 +3,17 @@ package Java.JavaFoundation.projects.project1;
 public class Main {
     public static void main(String[]args){
       LibraryManager libraryManager = new LibraryManager();
-   //   String bookName = input("Enter book name: ");
-      libraryManager.searchByTitle("test");  
-      
-      try{libraryManager.searchByTitle("test");}
-      catch(Exception e){
-        System.out.println(e.getMessage());
+      try{
+        libraryManager.searchByTitle("Java Programming");
+        libraryManager.searchByTitle("test"); 
       }
+      catch(DocumentNotFoundException e){
+        System.err.println(" error message  "+  e.getMessage());
+      }
+      catch(Exception e){
+        System.err.println(" unidentify system error message  "+  e.getMessage());
+      }
+   
       
       libraryManager.addDocument("B111", new Book("New book", 2024, "new author"));
       libraryManager.displayAll();

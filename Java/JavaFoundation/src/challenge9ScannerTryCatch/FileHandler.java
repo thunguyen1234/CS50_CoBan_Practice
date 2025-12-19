6608427be1f0285eb0d5b0a535da3dfbfc8b9685
Thu <thunt41@unifiedpost.com>
2025-12-19 16:28:33 +0700

    add lesson 1 java sensei

--- Java/JavaFoundation/src/challenge9ScannerTryCatch/FileHandler.java
+++ Java/JavaFoundation/src/challenge9ScannerTryCatch/FileHandler.java
@@ -4,16 +4,26 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+
 public class FileHandler {
 
-    public void readProfile(String fileName) throws FileNotFoundException, IOException {
-        FileReader reader = new FileReader(fileName);
+    public void readProfile(String fileName) {
+      //Khai báo tài nguyên reader trong try-with-resources
+       try(FileReader reader = new FileReader(fileName))
+    {
+        // 2. Logic chính được đặt ở đây.
         System.out.println("File " + fileName + " opened successfully.");
-        if (fileName.equals("profile.txt")) {
-            System.out.println("found profile name");
-
-        }
-        reader.close();
+        // ... (các lệnh đọc dữ liệu) ...
+        if(fileName.equals("profile.txt")) {
+           System.out.println("File " + fileName + " found.");
+        }   
+    }
+      catch(FileNotFoundException e){
+        System.err.println(" internal method error: file not found  "+ fileName+ "  continue");
+      }
+      catch(IOException e){
+        System.err.println("error I/O undefined file  "+ fileName+  e.getMessage());
 
     }
+}
 }
--- Java/JavaFoundation/src/challenge9ScannerTryCatch/Main.java
+++ Java/JavaFoundation/src/challenge9ScannerTryCatch/Main.java
@@ -1,18 +1,13 @@
 package challenge9ScannerTryCatch;
 
+
 public class Main {
     public static void main(String[] args) {
     FileHandler fileHandler = new FileHandler();
- try{
-    fileHandler.readProfile("profile.txt");
-     fileHandler.readProfile("data.txt");
- }
- catch(Exception e){
-    System.err.println(" error message  "+  e.getMessage());
- }
- finally{
-    System.out.println("Execution completed.");
- }
+     
+  fileHandler.readProfile("profile.txt");
+  fileHandler.readProfile("data.txt");
+
 }
 
 }
